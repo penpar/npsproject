108b60d55c931cb109ff377868f02c489b410629
Kim-Jack <works1790@naver.com>
2023-05-01 05:04:40 +0900

    Update npsproject.sql

--- npsproject.sql
+++ npsproject.sql
@@ -1,19 +1,3 @@
--- --------------------------------------------------------
--- 호스트:                          ls-5a1e15182828961dc315bb4ab58ec3cfc8390c41.comxvaxo6dcn.ap-northeast-2.rds.amazonaws.com
--- 서버 버전:                        8.0.32 - Source distribution
--- 서버 OS:                        Linux
--- HeidiSQL 버전:                  12.4.0.6659
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 
 -- j_nps 데이터베이스 구조 내보내기
 CREATE DATABASE IF NOT EXISTS `j_nps` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
@@ -61,10 +45,3 @@ CREATE TABLE IF NOT EXISTS `survey_type` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 테이블 데이터 j_nps.survey_type:~0 rows (대략적) 내보내기
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
